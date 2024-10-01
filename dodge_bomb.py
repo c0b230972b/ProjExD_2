8b7f24d786e23f64e0075fad6b1d28b8d761ba5b
c0b23097 <c0b230972b@edu.teu.ac.jp>
2024-10-01 17:32:41 +0900

    時間経過とともに爆弾の拡大、加速(途中)

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -33,6 +33,17 @@ def game_over(t) -> int:
     引数：GameOver画面を表示する秒数
     """
     return time.sleep(t)
+
+def expan_bb():
+    bb_accs = [a for a in range(1, 11)]  # 加速度のリスト
+    bb_imgs= []
+    for r in range(1, 11):
+        bb_img = pg.Surface((20*r, 20*r))
+        pg.draw.circle(bb_img,(255, 0, 0), (10*r, 10*r), 10*r)
+        bb_img.set_colorkey((0, 0, 0))
+        bb_imgs.append(bb_img)
+    return bb_accs, bb_imgs
+
     
 
 def main():
@@ -42,14 +53,19 @@ def main():
     kk_img = pg.transform.rotozoom(pg.image.load("fig/3.png"), 0, 0.9)
     kk_rct = kk_img.get_rect()
     kk_rct.center = 300, 200 
-    bb_img = pg.Surface((20, 20))  # 空のSurface
-    bb_img.set_colorkey((0, 0, 0))  # 爆弾の四隅を透過させる
-    pg.draw.circle(bb_img, (255, 0, 0), (10, 10), 10)
+    #bb_img = pg.Surface((20, 20))  # 空のSurface
+    #bb_img.set_colorkey((0, 0, 0))  # 爆弾の四隅を透過させる
+    #pg.draw.circle(bb_img, (255, 0, 0), (10, 10), 10)
+    
+    
+    vx, vy = +5, +5  # 爆弾の速度
+    bb_accs, bb_imgs = expan_bb()
+    bb_img = bb_imgs[min(tmr//500, 9)]
+    go_img = pg.Surface((WIDTH,HEIGHT))
+    bb_img = bb_imgs[0]
     bb_rct = bb_img.get_rect()  # 爆弾Rectの抽出
     bb_rct.centerx = random.randint(0, WIDTH)
     bb_rct.centery = random.randint(0, HEIGHT)
-    vx, vy = +5, +5  # 爆弾の速度
-    go_img = pg.Surface((WIDTH,HEIGHT))
     pg.draw.rect(go_img,(0, 0, 0), (0, 0, WIDTH, HEIGHT)) #(Surface, (色), (左上のX, 左上のy, 横幅, 縦幅))
     go_img.set_alpha(200)
     go_rct = go_img.get_rect()
@@ -77,7 +93,7 @@ def main():
             screen.blit(ck_img, ck_rct)
             screen.blit(ck_img2, ck_rct2)
             screen.blit(txt,[WIDTH//3 +30, HEIGHT//2 -20])
-            pg.display.flip()
+            pg.display.update()
             game_over(5)
             return
 
@@ -98,14 +114,19 @@ def main():
         kk_rct.move_ip(sum_mv)
         if check_bound(kk_rct) != (True, True):
             kk_rct.move_ip(-sum_mv[0], -sum_mv[1])
+
+        
         screen.blit(kk_img, kk_rct)
 
-        bb_rct.move_ip(vx, vy)
+        
         yoko, tate = check_bound(bb_rct)
         if not yoko:
             vx *= -1
         if not tate:
             vy *= -1
+        avx = vx*bb_accs[min(tmr//500, 9)]
+        avy = vy*bb_accs[min(tmr//500, 9)]
+        bb_rct.move_ip(avx, avy)
         screen.blit(bb_img, bb_rct)
         pg.display.update()
         tmr += 1
